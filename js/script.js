0235f37d68abb63d962feca2c51781deaa688e03
Saranya Shankar <saranya.shankar@yahoo.com>
2024-11-22 13:56:20 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function displayTitle(){
+    alert('Welcome to the Guess the number Game:')
+}
+
+function playGame(){
+    let number = Math.floor(Math.random()* 10 + 1)
+    let count = 1
+    let guess 
+    alert('I\'m guessing a number between 1 and 10')
+
+    while(true){
+        guess = Number(prompt('Enter your guess'))
+        if(number > guess){
+            alert('Too low')
+            count++
+        }else if(number < guess){
+            alert('Too high')
+            count++
+        }else if(number === guess){
+            alert(`You guessed it! It took you ${count} tries`)
+            return
+        }
+    }
+
+}
+
+function main(){
+    displayTitle()
+    let playAgain = 'y'
+    while(playAgain === 'y'){
+        playGame()
+        playAgain = prompt('Do you want to play again?(y or n)')
+    }
+    alert('Thanks for playing the game!')
+}
+main()
